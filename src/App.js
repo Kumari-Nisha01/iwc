1ac4bd987779d07608272f760543b31fc353dc12
Kumari-Nisha01 <77829731+Kumari-Nisha01@users.noreply.github.com>
2022-01-25 15:08:51 +0530

    iwc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import React from "react";
 import Switch from '@material-ui/core/Switch';
+import Close from '@material-ui/icons/Close'
 import KeyboardArrowUp from '@material-ui/icons/KeyboardArrowUp';
+import image from './images/img1.jpg'
 
 function App() {
   
@@ -10,10 +12,16 @@ function App() {
     <div className="heading">
       ROI Calculator   
       </div>
+      <span className='cut'><Close/></span>
+      <span className='img'>
+      <img className='img' src={image} alt="img"/>
+      </span>
+      <span className='imgt'>CAKE</span>
       <div className="switch">
-      <Switch color="warning" defaultChecked /> USD
+      <Switch defaultChecked color="warning" /> USD
       </div>
-      <input type="text" placeholder="0.000 USD" style={{textAlign:'right', padding:'2px 10px'}}/>
+      <input type="text" placeholder="0.000 USD"
+      style={{textAlign:'right', fontColor:'black', padding:'2px 10px'}}/>
       <div>
       <span className="span1">$1000</span>
       <span className="span2">$100</span>
@@ -28,27 +36,36 @@ function App() {
       <span className="span4">1 Year</span>
       <span className="span4">5 Year</span>
       </div>
-      <div style={{margin:'20px 0px', fontWeight:600}}>
+      <div style={{margin:'20px 0', fontWeight:600}}>
       Enable Accelarate APY
       </div>
-      <span className="slide">
-      <Switch color="warning" defaultChecked />
+      <span className="slide2">
+      <Switch defaultChecked color="warning" />
       </span>
+     
+      
       <div className=""> 
       <div style={{marginBottom:'10px', color:'gray'}}>Select Tier</div>
-      <span className="span3">Tier 1</span>
-      <span className="span4">Tier 2</span>
-      <span className="span4">Tier 3</span>
-      <span className="span4">Tier 4</span>
-      <span className="span4">Tier 5</span>
+      <span className="span5">Tier 1</span>
+      <span className="span6">Tier 2</span>
+      <span className="span6">Tier 3</span>
+      <span className="span6">Tier 4</span>
+      <span className="span6">Tier 5</span>
       <div className="sidetext2">  ROI at Current Rates</div>
       </div>
 
       <input className="input2"  type="text" placeholder="0.000 USD" style={{textAlign:'right', padding:'2px 10px'}}/>
       <div className="sidetext3">  ~ 0.000 CAKE + 0.000000 DON</div>
       <div style={{marginLeft:'200px', marginTop:'20px'}}>
-      <span style={{marginTop:'-20px', fontWeight:'600'}}>Hide Detailts </span>
+      <span style={{marginTop:'-20px', fontWeight:'600', fontSize:'14px'}}>Hide Detailts </span>
       <span><KeyboardArrowUp /></span></div>
+      <div style={{fontWeight:600}}>
+      APY
+      </div>
+      <div className="nine">9.0%</div>
+      <p className="p1">● Calculator based on current rates</p>
+      <p className="p2">● All figures are estimates provided for your convenience only, and by no <br/> <span style={{marginLeft:'12px'}}> means represent 
+      represent guaranteed returns.</span></p>
       </div>
   );
 }
